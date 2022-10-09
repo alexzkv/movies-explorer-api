20bc450f466b339d2c815bae82cb53958c7ce547
Захаров Александр Александрович <AlekAleZakharov@vimpelcom.ru>
2022-10-09 22:30:43 +0300

    feat: create schemas

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Введите корректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+// userSchema.methods.toJSON = function () {
+//   const user = this.toObject();
+//   delete user.password;
+//   return user;
+// };
+
+module.exports = mongoose.model('user', userSchema);
